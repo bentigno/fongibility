5d5ce5d5e766696f029054cfa95bc6fab64d9347
bentigno <100848498+bentigno@users.noreply.github.com>
2026-06-08 16:02:28 +0000

    Fix: bonne URL de production

--- frontend/src/api/ApiClient.ts
+++ frontend/src/api/ApiClient.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 //const API_BASE_URL = 'http://localhost:8080/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+//const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = 'https://fongibility-backend.onrender.com/api';
 
 
 interface AuthResponse {
